67c344c1e7beeff99eac365d4623116c0c53a615
egs33 <egs33.bicycle@gmail.com>
2020-07-04 17:22:14 +0900

    Add link cpp and go

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,17 @@
 #include <dhcp/hwaddr.h>
 #include <log/message_initializer.h>
 #include <log/macros.h>
+#include "../go_dist/libdynamic_lifetime_go.h"
 
 isc::log::Logger logger("dynamic_lifetime");
 
 extern "C" {
     int version() {
-        return KEA_HOOKS_VERSION;
+        int v = keaVersion();
+        if (v < 0) {
+            return KEA_HOOKS_VERSION;
+        }
+        return v;
     }
 
     int load(isc::hooks::LibraryHandle& handle) {
